b0b19094e7292686408508e3d93520eb6aa515e4
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2019-03-03 12:41:48 +0200

    Added task - Subtraction.

---
+++ 12.DOMManipulation-Exercises/01.Subtraction/subtract.js
@@ -0,0 +1,7 @@
+function subtract() {
+    let firstNumber = Number($('#firstNumber').val());
+    let secondNumber = Number($('#secondNumber').val());
+
+    let result = firstNumber - secondNumber;
+    $('#result').text(result);
+}
