7ac0cb7e375fea44e7339af61e3769940ae30981
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2019-03-04 15:07:15 +0200

    Removed tasks.

--- 12.DOMManipulation-Exercises/05.Notification/notification.js
+++
@@ -1,8 +0,0 @@
-function notify(message) {
-    $('#notification').css('display', 'block');
-    $('#notification').text(message);
-
-    setTimeout(function () {
-        $('#notification').css('display', 'none');
-    }, 2000);
-}
