1d711a9928001a8f48efe7ed5e9f01e0382180ec
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2019-03-09 19:53:07 +0200

    Uploaded exam - 15.07.2018

---
+++ Exam-15.07.2018/01.Notes/solution.js
@@ -0,0 +1,36 @@
+/// <reference path="../typings/globals/jquery/index.d.ts" />
+
+function addSticker() {
+    let $title = $('input.title');
+    let $content = $('input.content');
+
+    let title = $title.val();
+    let content = $content.val();
+    
+    if (title && content) {
+        let $li = $('<li>');
+        $li.attr('class', 'note-content');
+
+        let $a = $('<a>');
+        $a.attr('class', 'button');
+        $a.text('x');
+        $a.on('click', function(){
+            $li.remove();
+        });
+        $li.append($a);
+
+        let $h2 = $(`<h2>${title}</h2>`);
+        $li.append($h2);
+
+        $li.append('<hr>');
+
+        let $p = $(`<p>${content}</p>`);
+        $li.append($p);
+
+        let $ul = $('#sticker-list');
+        $ul.append($li);    
+    }
+
+    $title.val('');
+    $content.val('');
+}
